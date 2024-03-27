31b38b7b1869d05a6ed845798838a03fb5b5cd7c
Alexey <alexeishibakov@yandex.ru>
2024-03-27 16:18:16 +0300

    HW 2 peredelka

--- catalog/models.py
+++ catalog/models.py
@@ -1,29 +1,15 @@
 from django.db import models
 
 
-class BaseModel(models.Model):
-    """
-    Абстрактная модель, предоставляющая поля для отслеживания даты создания
-    и последнего изменения объекта.
-    """
-    created = models.DateTimeField(
-        auto_now_add=True,
-        verbose_name='дата создания')
-    changed = models.DateTimeField(
-        auto_now=True,
-        verbose_name='дата последнего изменения')
-
-    class Meta:
-        abstract = True
-
-
 class Category(models.Model):
     """
     Модель категории товаров.
     """
+
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     ordering = ('title',)
+
 
     def __str__(self):
         return f'{self.title}: {self.description}'
@@ -33,9 +19,11 @@ class Category(models.Model):
         verbose_name_plural = 'категории'
 
 
-class Product(BaseModel):
+class Product(models.Model):
     """
     Модель продукта.
+    Абстрактная модель, предоставляющая поля для отслеживания даты создания
+    и последнего изменения объекта.
     """
     title = models.CharField(max_length=100, verbose_name='наименование')
     description = models.TextField(
@@ -52,6 +40,13 @@ class Product(BaseModel):
         blank=True,
         null=True)
     price = models.IntegerField(verbose_name='цена за покупку')
+
+    created = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='дата создания')
+    changed = models.DateTimeField(
+        auto_now=True,
+        verbose_name='дата последнего изменения')
 
     def __str__(self):
         return f'{self.title}: {self.description} - {self.price}'
